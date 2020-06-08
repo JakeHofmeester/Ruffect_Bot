256ac544ab183f37e1b776e654240e132b16ae02
LittleBunnyRabbit <47859473+LittleBunnyRabbit@users.noreply.github.com>
2020-06-08 11:18:56 +0200

    Created a basic structure (#1) (#1)
    Co-authored-by: Andraž Mesarič-Sirec <58178084+AndrazWhileTrue@users.noreply.github.com>

---
+++ src/bot/commands/Example.js
@@ -0,0 +1,13 @@
+/**
+ * This is a basic command layout
+ * You export command name the function that will be executed when the command is called
+ * in this case it will call @function Example
+ */
+module.exports = {
+    name: "Example",
+    execute: Example
+};
+
+function Example(msg, args) {
+    return msg.channel.send(`Hello! Your arguments: ${args.join(", ")}`);
+}
---
+++ src/utils/Imports.js
@@ -0,0 +1,16 @@
+/**
+ * I prefer to have all npm modules in one place
+ * The idea behind it is that you import them like this:
+ * @example
+ * const {
+ *    Discord,
+ *    request,
+ *    fs
+ * } = require("./Imports");
+ */
+
+module.exports = { 
+    Discord: require("discord.js"),
+    request: require("request"),
+    fs: require("fs")
+}

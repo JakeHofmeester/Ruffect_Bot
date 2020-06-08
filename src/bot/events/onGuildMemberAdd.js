530321ea95b462d8017d49d10db1e814432b3d86
Jake <jakehofmeester@hotmail.com>
2020-06-08 17:12:22 +0200

    Added auto role add + changed platform avatars

--- src/bot/events/onGuildMemberAdd.js
+++ src/bot/events/onGuildMemberAdd.js
@@ -2,4 +2,6 @@ module.exports = function (member) {
   member.guild.channels.cache
     .get("701860346897498113")
     .send(`Hey ${member}, welcome to **Ruffect's Server**! `);
+
+  member.roles.add(global.gConfig.default_role);
 };
